38b3389d345e610cd9fbe8252230eda38284effa
cmarcelos <claudiomarcelosilva28@gmail.com>
2024-05-14 17:18:50 -0300

    Criando Calculadora e Comentarios

---
+++ tipos-variaveis/src/Comentarios.java
@@ -0,0 +1,26 @@
+public class Comentarios {
+    public static void main(String[] args) {
+        // Olá, eu sou um comentário em uma única linha
+
+        /* Olá,
+        * Eu sou um comentario
+        * que posso ser mais detalhadod
+        * quando necessário
+        */
+
+    }
+    public void metodo(){
+        /** 
+        * Estas duas estrelinhas acima
+        * é para identificar que você
+        * pretende elaborar um comentário
+        * a nível de documentação.
+        * Que incrível !!!
+        */
+
+        // Para criar nossa documentação no formato html, para disponibilizar via web.
+        // No terminal execute o comando abaixo
+        //javadoc -encoding UTF-8 -docencoding ISO-8859-1  -d ../docs  src/*.java         
+    
+    }
+}
