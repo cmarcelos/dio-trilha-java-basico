adc38f143fd55182166202ca19720814232539b1
cmarcelos <claudiomarcelosilva28@gmail.com>
2024-05-14 13:03:08 -0300

    Operadores

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,20 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        //tipos primitivos
+        //estude a classe String que representa texto
+
+        String meuNome = "Claudio Silva";
+        double salarioMinimo = 2500;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        int numero = 1;
+
+        numero = 2;
+
+        System.out.println(numero);
+
     }
 }
