f924e34ef8695260ac06379e51644d12061671ea
cmarcelos <claudiomarcelosilva28@gmail.com>
2024-05-23 18:14:50 -0300

    Estudos das estruturas

---
+++ tipos-variaveis/src/EstrutRepeticaoExemploForEach.java
@@ -0,0 +1,10 @@
+public class EstrutRepeticaoExemploForEach {
+    public static void main(String[] args) {
+        // em arrays o indice inicia em ZERO
+        String alunos[] = { "FELIPE", "JONAS", "JULIA", "MARCOS" };
+        
+        for(String aluno : aluno){
+            System.out.println("Nome do aluno é: " + aluno);
+        }
+    }
+}
